91b7cc941e8ce7bcfffb290bda632540b7e2db75
amirr-k <damianwritescode@gmail.com>
2025-07-06 23:18:39 -0700

    Not working but tracking progress

--- frontend/src/components/recorder.js
+++ frontend/src/components/recorder.js
@@ -2,8 +2,21 @@ import React from 'react';
 import microphone from '../assets/a7f5a253d2b3ff0e994ecbac333f8a406bf598d27f812e18e69070fc1d4ddf4e.jpg';
 import './recorder.css';
 const Recorder = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  function startRecording() = async () => {
+    setIsRecording(true);
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+    mediaRecorder.onstop = () => {
+      setIsRecording(false);
+  }
+  function stopRecording() {
+    setIsRecording(false);
+  }
   return (
     <button className="recorder">
+      {isRecording ? (
       <img src={microphone} alt="microphone" className="microphone" />
     </button>
   );
