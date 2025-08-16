24b39a0f624fec6ab11b6c4be5cc43d3311ada51
Amir <damianwritescode@gmail.com>
2025-08-15 20:22:45 -0700

    Finished writing the constructor and the saveAudio function

--- audio_service.cpp
+++ audio_service.cpp
@@ -8,3 +8,27 @@
 #include <httplib.h>
 
 using namespace std;
+
+class AudioService {
+    private:
+        string outputDirectory;
+    public:
+        AudioService(const string &directoryName){
+            outputDirectory = directoryName;
+            string command = "mkdir -p " + outputDirectory;
+            system(command.c_str());
+        }
+        bool saveAudio(vector <char> audioData, string fileName){
+            string filePath = outputDirectory + "/" + fileName;
+            ofstream file(filePath.c_str(), ios::binary);
+            if (!file.is_open()){
+                cerr << "Failed to create file: " << filePath << endl;
+                return false;
+            }
+            for (int i = 0; i < audioData.size(); ++i){
+                file << audioData[i];
+            }
+            file.close();
+            printf("Audio saved to %s\n", filePath.c_str());
+            return true;
+        }
