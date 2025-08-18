39518040b7dad22e83f63eae9224a2027e5992c2
Amir <damianwritescode@gmail.com>
2025-08-18 16:58:49 -0700

    Wrote code to forward the audio to the audio service

--- lambda_function.py
+++ lambda_function.py
@@ -2,3 +2,21 @@ import json
 import boto3
 import requests
 import base64
+
+def lambda_handler(event, context):
+    #Get audio from API Gateway
+    try:
+        if event.get("isBase64Encoded"):
+            #Convert to binary
+            audio_data = base64.b64decode(event["body"])
+        else:
+            body = event["body"]
+            #Already in binary format
+    
+    #Send audio to C++ service
+    #Need to make this dynamic later
+    cppServiceUrl = "http://localhost:8080/process"
+
+    response = response.post(cppServiceUrl, files = {"audio": ("record.wav", body, "audio/wav")})
+
+    
