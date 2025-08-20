2d911131cb0ba469bce4d00f8007d3e07ff9b0a0
Amir <damianwritescode@gmail.com>
2025-08-19 18:06:16 -0700

    Fixed some syntax issues

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,14 @@
 import json
 import boto3
-import requests
 import base64
+from datetime import datetime
 
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
     #Get audio from API Gateway
     try:
+        # Get audio from API Gateway
         if event.get("isBase64Encoded"):
             #Convert to binary
             audioData = base64.b64decode(event["body"])
@@ -19,24 +20,27 @@ def lambda_handler(event, context):
         filename = f"audio_{timestamp}.wav"
 
         s3_client.put_object(
-                Bucket='voice-recordings-bucket-amirkiadi-2025',
-                Key=filename,
-                Body=audio_data,
-                ContentType='audio/wav'
-            )
+            Bucket='voice-recordings-bucket-amirkiadi-2025',
+            Key=filename,
+            Body=audioData,
+            ContentType='audio/wav'
+        )
         
         return {
-                'statusCode': 200,
-                'headers': {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json'
-                },
-                'body': json.dumps({
-                    'message': f'Audio uploaded successfully: {filename}',
-                    'status': 'success'
-                })
-            }
-
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'message': f'Audio uploaded successfully: {filename}',
+                'status': 'success'
+            })
+        }
         
     except Exception as e:
-        return {'statusCode': 500,'headers': {'Access-Control-Allow-Origin': '*'},'body': json.dumps({'error': str(e)})}
+        return {
+            'statusCode': 500,
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'body': json.dumps({'error': str(e)})
+        }
