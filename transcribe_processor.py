481853c3232df8b329afedda5f1b74b2cd1e8f13
Amir <damianwritescode@gmail.com>
2025-08-19 18:10:52 -0700

    Finished transcribe processor

--- transcribe_processor.py
+++ transcribe_processor.py
@@ -4,3 +4,30 @@ import urllib.parse
 
 transcribe = boto3.client('transcribe')
 s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    try:
+        # Get bucket and key from S3 event
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+
+        # Start transcription job
+        jobName = f"transcribe-{key.replace('/', '-').replace('.', '-')}"
+        jobURI = f"s3://{bucket}/{key}"
+        
+        transcribe.start_transcription_job(
+            TranscriptionJobName=jobName,
+            Media={'MediaFileUri': jobURI},
+            MediaFormat='wav',
+            LanguageCode='en-US',
+            OutputBucketName=bucket
+        )
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Transcription job started')
+        }
+
+    except Exception as e:
+        print(f"Error: {e}")
+        raise e
